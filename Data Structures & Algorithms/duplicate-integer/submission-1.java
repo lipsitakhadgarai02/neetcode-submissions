a91b1c5e4553f028923814a6097d22ab0399d87f
Lipsita Khadgarai <lipsitakhadgarai16@gmail.com>
2026-05-31 16:22:51 +0530

    Bulk sync: 4 submissions

---
+++ Data Structures & Algorithms/duplicate-integer/submission-1.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+        java.util.Arrays.sort(nums);
+        int count = 0;
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (nums[i] == nums[i + 1])
+                count++;
+        }
+        if (count == 0) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+}
